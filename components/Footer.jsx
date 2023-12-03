d381a89e2dfa5ac1051930d37701735bfd9e5a60
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2023-12-03 09:28:26 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
         &copy; Design And Devlop by <a target='_blank' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08">@Jagannath</a>.
     </small>
     <p className='text-gray-700 flex justify-center mt-1 gap-5 text-sm font-semibold'>Lets catch up - 
-        <a className='mt-1 text-lg' href=""><FaXTwitter/></a>
-        <a className='mt-1 text-lg' href=""><FaInstagram/></a>
-        <a className='mt-1 text-lg' href=" "><FaLinkedinIn/></a>
+        <a className='mt-1 text-lg' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08"><FaXTwitter/></a>
+        <a className='mt-1 text-lg' href="https://instagram.com/mr.shingne_2161?igshid=MzMyNGUyNmU2YQ=="><FaInstagram/></a>
+        <a className='mt-1 text-lg' href=" https://www.linkedin.com/in/jagannath-shingne-201aba236"><FaLinkedinIn/></a>
      </p>
    </footer>
   )
