d229ebcf9f7ccd2ee954e7bb0151fd0261690263
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-12-03 20:53:45 +0530

    footer section added navbar  section edit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
   return (
    <footer className='text-center mb-4 px-4'>
     <small className='text-gray-500   '>
-        &copy; Design And Devlop by <a target='_blank' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08">@Jagannath</a>.
+        &copy; Design And Devlop by <a target='_blank' href="">@Jagannath</a>.
     </small>
     <p className='text-gray-700 flex justify-center mt-1 gap-5 text-sm font-semibold'>Lets catch up - 
-        <a className='mt-1 text-lg' href=""><FaXTwitter/></a>
+        <a className='mt-1 text-lg' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08"><FaXTwitter/></a>
         <a className='mt-1 text-lg' href=""><FaInstagram/></a>
         <a className='mt-1 text-lg' href=" "><FaLinkedinIn/></a>
      </p>
