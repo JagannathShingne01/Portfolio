26b82e98dc353714b2fa7a976e88802ced11534b
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-12-12 22:48:14 +0530

    toaster activated

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import toast from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast';
 import { FaPaperPlane } from 'react-icons/fa';
 import {useInView} from 'react-intersection-observer'
 import { ActiveSectionContex } from '@/context/active-context';
@@ -58,7 +58,7 @@ const Contact = () => {
                     body: JSON.stringify(contactData),
                   });
                 toast.success(
-                    "Your enquiry has been submitted successfully. We'll get back to you at our earliest.",
+                    "Your Message has been submitted successfully. I'll get back to you at my earliest.",
                     { duration: 3000 }
                 );
               }catch(e){
@@ -76,6 +76,7 @@ const Contact = () => {
         return(
             <div id='contact' ref={ref} className=' scroll-mt-28 mb-6'>
                 <section  className='text-gray-700 dark:text-white/80'>
+                    <Toaster/>
                     <h1 className=' text-5xl flex justify-center items-center'>Contact Me</h1>
                     <p className='mt-2 md:mt-3 mb-10 text-sm md:flex items-center justify-center mx-3 text-center'>
                         Please Contact me directly at  &nbsp;  <a className='underline' href="mailto:jagannathshingne01@gmail.com">jagannathshingne01@gmail.com</a> &nbsp;  or through this form.
@@ -168,7 +169,7 @@ const Contact = () => {
                                     <textarea name="message"
                                             id="message"
                                             value={contactData.message}
-                                            className='border border-black/10 h-50 rounded-lg p-4 w-full my-2 '
+                                            className='border border-black/10 h-50 rounded-lg p-4 w-full my-2 text-gray-900 '
                                             onChange={(e)=>
                                                 setContactData((prev)=>({
                                                     ...prev,
