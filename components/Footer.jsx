e8b0da1a569f354f3fa6483b2e0e319787dc8e4c
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-03-22 20:10:24 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
    <footer className='text-center mb-4 px-4'>
   <div className='border-t-2 mx-2 md:mx-44'></div>
     <p className={`${roboto} text-gray-700 flex justify-center mt-1 gap-5 text-sm font-semibold `}>lets catch up -
-        <a className='mt-1 text-lg' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08"><FaXTwitter/></a>
-        <a className='mt-1 text-lg' href="https://www.instagram.com/mr.shingne_2161/?igshid=MzMyNGUyNmU2YQ%3D%3D"><FaInstagram/></a>
-        <a className='mt-1 text-lg' href="https://www.linkedin.com/in/jagannath-shingne-201aba236"><FaLinkedinIn/></a>
+        <a target='_blank' className='mt-1 text-lg' href="https://x.com/MrShingne?t=8y5ycfGfNCP80sQ-jmOTYA&s=08"><FaXTwitter/></a>
+        <a target='_blank' className='mt-1 text-lg' href="https://www.instagram.com/mr.shingne/?igshid=MzMyNGUyNmU2YQ%3D%3D"><FaInstagram/></a>
+        <a target='_blank' className='mt-1 text-lg' href="https://www.linkedin.com/in/jagannath-shingne-201aba236"><FaLinkedinIn/></a>
      </p>
      <small className='text-gray-500   '>
         &copy; Design And Devlop by Jagannath Shingne.  
