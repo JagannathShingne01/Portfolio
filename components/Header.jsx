cfa8f6f7553abced1024960d6d26d4708234cca5
JagannathShingne01 <jagannathshingne01@gmail.com>
2026-02-08 13:15:07 +0530

    Update resume link and replace with new PDF file

--- components/Header.jsx
+++ components/Header.jsx
@@ -118,7 +118,7 @@ export default function Header() {
 
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 mt-8 justify-center">
             <a
-              href="/JagannathDEVResume.pdf"
+              href="/JagannathResumeSDE.pdf"
               download
               target="_blank"
               rel="noopener noreferrer"
